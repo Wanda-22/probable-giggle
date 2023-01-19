eeae6c1bbdb36f421eab3e6287bd1a27748b76aa
Wanda-22 <105216373+Wanda-22@users.noreply.github.com>
2023-01-19 18:11:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,39 @@ function start() {
 }
 
 function modelloaded() {
-    console.log("Model is loaded.");
+    console.log("It is loaded.");
+    m.classify(Answer);
+}
+
+
+function Answer(error,result) {
+    if (error) {
+        console.log(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById('ear').innerHTML="I can hear - " + result[0].label;
+        document.getElementById('acc').innerHTML="Accuracy - " + Math.floor(result[0].confidence*100)+ "%";
+        R=Math.floor(Math.random()*255)+1;
+        B=Math.floor(Math.random()*255)+1;
+        G=Math.floor(Math.random()*255)+1;
+        document.getElementById('ear').style.color="rgb("+R+","+B+","+G+")";
+        document.getElementById('acc').style.color="rgb("+R+","+B+","+G+")";
+        a1=document.getElementById('a');
+        if (result[0].label=="Meowing") {
+            a1.src="cat.jpg";
+        }
+        else if (result[0].label=="Moowing") {
+            a1.src="cow.jpg";
+        }
+        else if (result[0].label=="Chirping") {
+            a1.src="bird.png";
+        }
+        else if (result[0].label=="Barking") {
+            a1.src="dog.png";
+        }
+        else {
+            a1.src="ear.jpg"
+        }
+    }
 }
